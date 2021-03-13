c1ff5a9523d55125cb09237860d1aed0130bc4ad
Will Carcamo <william.j.carcamo@gmail.com>
2021-03-12 21:03:22 -0800

    add states for forms

--- src/components/exercises.js
+++ src/components/exercises.js
@@ -1,71 +1,130 @@
 import React from 'react';
-import ExerciseForm from './exerciseform';
+// import ExerciseForm from './exerciseform';
 
 class Exercises extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {exercise: "Squat"};
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      exercise: "Squat",
+      lbs: 0,
+      sets: 3,
+      reps: 5,
+      bw: false,
+      tf: false,
+      gm: false
+    };
   }
 
   handleForm = (e) => {
     this.setState({exercise: e});
   }
 
+  handleInputChange(e) {
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  submitForm() {
+    var dat = [
+      this.state.exercise,
+      this.state.lbs,
+      this.state.sets,
+      this.state.reps,
+      this.state.bw,
+      this.state.tf,
+      this.state.gm
+    ];
+    alert('array: ' + dat);
+  }
+
   render() {
-    let exerciseForm;
-    switch (this.state.exercise) {
-      case "Squat":
-        exerciseForm = <ExerciseForm exercise={this.state.exercise} />;
-        break;
-      case "Bench Press":
-        exerciseForm = <ExerciseForm exercise={this.state.exercise} />;
-        break;
-      case "Chin Up":
-        exerciseForm = <ExerciseForm exercise={this.state.exercise} />;
-        break;
-      case "Overhead Press":
-        exerciseForm = <ExerciseForm exercise={this.state.exercise} />;
-        break;
-      case "Deadlift":
-        exerciseForm = <ExerciseForm exercise={this.state.exercise} />;
-        break;
-      case "Custom":
-        exerciseForm = <ExerciseForm exercise={this.state.exercise} />;
-        break;
-      default:
-        break;
-    }
     return (
       <div className="w3-row">
         <div className="w3-padding w3-quarter">
           <div className="w3-card w3-bar-block w3-light-gray">
             <button 
               className="w3-bar-item w3-button"
-              onClick={() => this.handleForm("Squat")}
-            >Squat</button>
+              onClick={() => this.handleForm("Squat")}>
+                Squat</button>
             <button 
               className="w3-bar-item w3-button"
-              onClick={() => this.handleForm("Bench Press")}
-            >Bench Press</button>
+              onClick={() => this.handleForm("Bench Press")}>
+                Bench Press</button>
             <button 
               className="w3-bar-item w3-button"
-              onClick={() => this.handleForm("Chin Up")}
-            >Chin Up</button>
+              onClick={() => this.handleForm("Chin Up")}>
+                Chin Up</button>
             <button 
               className="w3-bar-item w3-button"
-              onClick={() => this.handleForm("Overhead Press")}
-            >Overhead Press</button>
+              onClick={() => this.handleForm("Overhead Press")}>
+                Overhead Press</button>
             <button 
               className="w3-bar-item w3-button"
-              onClick={() => this.handleForm("Deadlift")}
-            >Deadlift</button>
+              onClick={() => this.handleForm("Deadlift")}>
+                Deadlift</button>
             <button 
               className="w3-bar-item w3-button"
-              onClick={() => this.handleForm("Custom")}
-            >Custom</button>
+              onClick={() => this.handleForm("Custom")}>
+                Custom</button>
           </div>
         </div>
-        {exerciseForm}
+        <div className="w3-padding w3-rest">
+          <div className="w3-card">
+            <form className="w3-container">
+              <h2>{this.state.exercise}</h2>
+              <label>lbs:</label>
+                <input 
+                  name="lbs"
+                  className="w3-input w3-border" 
+                  type="number" 
+                  value={this.state.lbs} 
+                  onChange={this.handleInputChange} />
+              <label>Sets:</label>
+                <input
+                  name="sets"
+                  className="w3-input w3-border" 
+                  type="number" 
+                  value={this.state.sets}
+                  onChange={this.handleInputChange} />
+              <label>Reps:</label>
+                <input
+                  name="reps"
+                  className="w3-input w3-border" 
+                  type="number" 
+                  value={this.state.reps} 
+                  onChange={this.handleInputChange} />
+              <input 
+                name="bw"
+                className="w3-check" 
+                type="checkbox" 
+                onChange={this.handleInputChange} />
+                <label className="w3-margin-left">BodyWeight</label>
+              <br></br>
+              <input 
+                name="tf"
+                className="w3-check" 
+                type="checkbox" 
+                onChange={this.handleInputChange} />
+                <label className="w3-margin-left">To Failure</label>
+              <br></br>
+              <input 
+                name="gm"
+                className="w3-check" 
+                type="checkbox" 
+                onChange={this.handleInputChange} />
+                <label className="w3-margin-left">Goal Met</label>
+              <br></br>
+              <button className="w3-button w3-blue-gray w3-section" onClick={this.submitForm}>Submit</button>
+            </form>
+          </div>
+        </div>
       </div>
       
     );
